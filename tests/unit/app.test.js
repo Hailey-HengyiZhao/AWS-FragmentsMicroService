ac64a39370b074d03c2f3675601c572284eaaf2c
Hengyi Zhao <101903534+Hailey-HengyiZhao@users.noreply.github.com>
2023-05-28 22:23:05 -0400

    App.js modified uncovered code line 40

---
+++ tests/unit/app.test.js
@@ -0,0 +1,20 @@
+// tests/unit/app.test.js
+
+const request = require('supertest');
+
+// Get our Express app object (we don't need the server part)
+const app = require('../../src/app');
+
+describe('404 Status Code Check', () => {
+  test('should return HTTP 404 response', async () => {
+    const res = await request(app).get('/non-existing-routes');
+    expect(res.statusCode).toBe(404);
+    expect(res.response.body).toEqual({
+      status: 'error',
+      error: {
+        message: 'not found',
+        code: 404,
+      },
+    });
+  });
+});
