db7c0764ca4eed6c863fe356f633eb38dcde033b
Hengyi Zhao <101903534+Hailey-HengyiZhao@users.noreply.github.com>
2023-05-28 22:26:09 -0400

    App.js modified uncovered code line 40

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -9,7 +9,7 @@ describe('404 Status Code Check', () => {
   test('should return HTTP 404 response', async () => {
     const res = await request(app).get('/non-existing-routes');
     expect(res.statusCode).toBe(404);
-    expect(res.response.body).toEqual({
+    expect(res.body).toEqual({
       status: 'error',
       error: {
         message: 'not found',
