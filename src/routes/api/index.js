84b738ea109e08dc75ebde21b43e9e753fbed411
Hengyi Zhao <101903534+Hailey-HengyiZhao@users.noreply.github.com>
2023-06-10 17:08:08 -0400

    updating POST /v1/fragments

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -11,8 +11,8 @@ const router = express.Router();
 // Define our first route, which will be: GET /v1/fragments
 router.get('/fragments', require('./get'));
 
-// POST /V1/fragments
-router.post("/fragments",require("./post") );
+// // POST /V1/fragments
+// router.post("/fragments",require("./post") );
 
 // Other routes will go here later on...
 module.exports = router;
