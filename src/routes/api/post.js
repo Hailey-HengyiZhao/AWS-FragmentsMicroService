44620310e27c34461c16e045016b14c70803dad7
Hengyi Zhao <101903534+Hailey-HengyiZhao@users.noreply.github.com>
2023-06-11 19:07:31 -0400

    update post.test with Error Content Type

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,14 +1,23 @@
-const { createSuccessResponse } = require('../../response');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
 const logger = require('../../logger');
 
 module.exports = async (req, res) => {
+
+  const type = req.get('Content-Type');
+
+  // Checking the Content-Type to be text
+  if (!Fragment.isSupportedType(type)) {
+    logger.error('415 Error: Not supported "Content-Type"');
+    return res.status(415).json(createErrorResponse(415, 'Not supported "Content-Type"'));
+  }
+
   const fragmentContent = req.body;
   const ownerId = req.user;
 
   const fragment = new Fragment({
     ownerId: ownerId,
-    type: req.get('Content-Type'),
+    type: type,
   });
 
   logger.debug('Create Fragment having ownerId: ' + fragment.ownerId + ' Type: ' + fragment.type);
@@ -28,5 +37,5 @@ module.exports = async (req, res) => {
     `${req.protocol}://${req.headers.host}/v1/fragments/${fragment.id}`;
   res.setHeader('Location', fragmentUrl);
 
-  res.status(200).json(createSuccessResponse({ fragment: fragment}));
+  res.status(200).json(createSuccessResponse({ fragment: fragment }));
 };
--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -1,8 +1,12 @@
 // tests/routes.test.js
 const request = require('supertest');
 const app = require('../../src/app'); 
+const hash = require('../../src/hash');
 
 describe('POST /v1/fragments', () => {
+
+   const email = "user1@email.com";
+
   test('unauthenticated requests are denied', async () => {
     const response = await request(app).post('/v1/fragments');
     expect(response.statusCode).toBe(401);
@@ -15,17 +19,29 @@ describe('POST /v1/fragments', () => {
     expect(response.statusCode).toBe(401);
   });
 
-  test('creates a new fragment', async () => {
+  test('incorrect Content-Type are denied', async()=>{
+    const fragmentData = "This is a sample fragment text.";
+
+    const res = await request(app)
+    .post('/v1/fragments')
+    .set('Content-Type', 'text/html')
+    .auth(email, 'password1') 
+    .send(fragmentData);
+
+    expect(res.statusCode).toBe(415);
+  })
+
+  test('Res fragment with correct Content-Type', async () => {
     const fragmentData = "This is a sample fragment text.";
 
     const res = await request(app)
       .post('/v1/fragments')
       .set('Content-Type', 'text/plain')
-      .auth('user1@email.com', 'password1') 
+      .auth(email, 'password1') 
       .send(fragmentData);
 
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('ok');
-    expect(res.body.fragment.ownerId).toEqual('user1@email.com');
+    expect(res.body.fragment.ownerId).toEqual(hash(email));
   });
 });
